2b2642de5251b450cfb76f2406b908d00cedc938
Bryon Burleigh <bryonb@gmail.com>
2015-02-02 17:44:24 -0800

    fix errors in alg1 and alg3 (alg3 still has 1 tiny bug) and make timings runner

--- Algo3.py
+++ Algo3.py
@@ -1,11 +1,8 @@
-ar = [1, 4, -9, 8, 1, 3, 3, 1, -1, -4, -6, 2, 8, 19, -10, -11]
-
-
 def maxCross(ar, low, mid, high):
     leftSum = 0
     tempSum = 0
     leftHigh = mid
-    for i in range(mid+1, low, -1):
+    for i in range(mid, low-1, -1):
         tempSum = tempSum + ar[i]
         if tempSum > leftSum:
             leftSum = tempSum
@@ -30,7 +27,7 @@ def maxSubArray(ar, low, high):
         mid = (high + low)/2
         (leftLow, leftHigh, leftSum) = maxSubArray(ar, low, mid)
         (rightLow, rightHigh, rightSum) = maxSubArray(ar, mid+1, high)
-        (crossLow, crossHigh, crossSum) = maxCrossing(ar, low, mid, high)
+        (crossLow, crossHigh, crossSum) = maxCross(ar, low, mid, high)
 
     if leftSum >= rightSum and leftSum >= crossSum:
         return(leftLow, leftHigh, leftSum)
@@ -39,8 +36,32 @@ def maxSubArray(ar, low, high):
     else:
         return(crossLow, crossHigh, crossSum)
 
-(low, high, maxSum) = maxSubArray(ar, 0, len(ar)-1)
+#(low, high, maxSum) = maxSubArray(ar, 0, len(ar)-1)
+#print(ar, ar[low:high], maxSum)
 
-print(ar, ar[low:high], maxSum)
-  
 
+'''
+#Instructor-supplied tests:
+A=[[1, 4, -9, 8, 1, 3, 3, 1, -1, -4, -6, 2, 8, 19, -10, -1],
+        [2, 9, 8, 6, 5, -11, 9, -11, 7, 5, -1, -8, -3, 7 -2],
+        [10, -11, -1, -9, 33,-45, 23, 24, -1, -7 -8, 19],
+        [31,-41, 59, 26, -53, 58, 97, -93, -23, 84 ],
+        [3, 2, 1, 1,-8, 1,1,2, 3],
+        [12, 99, 99, -99, -27, 0, 0, 0, -3,10 ],
+        [-2, 1,-3, 4, -1, 2, 1,-5, 4 ],
+        [-1, -3, -5 ]]
+
+for ar in A:
+    (low, high, maxSum) = maxSubArray(ar, 0, len(ar)-1)
+    print(ar, ar[low:high+1], maxSum)
+
+
+#Jen-supplied tests:
+maxSubArray([1, 2, 4, -1, 4, -10, 4, -19, 18, -1, -3, -4, 11, 3, -20, 19, -33, 50, 66, -22, -4, -55, 91, 100, -102, 9, 10, 19, -10, 10, 11, 11, -10, -18, 50, 90])
+maxSubArray([12, 12, 14, -88, -1, 45, 6, 8, -33, 2, 8, -9, -33, -8, -23, -77, -89, 1, 9, 10, 92, 87])
+maxSubArray([565, 78, 33, 9, 10, 84, 71, -4, -22, -55, -10, 76, -9, -9, -11, 76, 89, 11, 10, -33, 9])
+maxSubArray([2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3])
+maxSubArray([2])
+maxSubArray([-1, -1, -1, -1, -1, -100, -10, -10, 100, 100, 100, 100, -100, 100, 10, -10, -1])
+maxSubArray([12, 23, 44, -17, 12, 14, -88, -1, 45, 6, 8, -33, 2, 8, -9, -33, -8, -23, -77, -89, 1, 9, 13, -25, 10, 92, 57, 99, -22])
+'''
---
+++ timings.py
@@ -0,0 +1,140 @@
+import Algo1 as alg1
+import Algo2 as alg2
+import Algo3 as alg3
+
+import random
+import datetime
+
+#build arrays of random numbers
+A=[]
+for h in range(10):
+    A.append([])
+    for i in range(100, 1000, 100):
+        ar=[]
+        for j in range(i):
+            ar.append(random.randint(-100, 100))
+        A[h].append(ar)
+
+    for i in range(1000, 10000, 1000):
+        ar=[]
+        for j in range(i):
+            ar.append(random.randint(-100, 100))
+        A[h].append(ar)
+
+#do timings
+al1 = al2 = al3 = al4 = []
+
+for h in range(len(A)):
+    al1.append({})
+    al2.append({})
+    al3.append({})
+    al4.append({})
+    for i in range(len(A[h])):
+        #Algorithm 1
+        if i > 9:
+            time1 = datetime.datetime.utcnow()
+            alg1.maxSubArray(A[h][i])
+            time2 = datetime.datetime.utcnow()
+            finaltime = (time2 - time1)
+            al1[h][len(A[h][i])] = finaltime.microseconds
+        else:
+            al1[h][len(A[h][i])] = 0
+
+
+
+        #Algorithm 2
+        time1 = datetime.datetime.utcnow()
+        alg2.maxSubArray(A[h][i])
+        time2 = datetime.datetime.utcnow()
+        finaltime = (time2 - time1)
+        al2[h][len(A[h][i])] = finaltime.microseconds
+
+        #Algorithm 3
+        time1 = datetime.datetime.utcnow()
+        alg3.maxSubArray(A[h][i])
+        time2 = datetime.datetime.utcnow()
+        finaltime = (time2 - time1)
+        al3[h][len(A[h][i])] = finaltime.microseconds
+
+
+#get averages for alg1
+averages = {}
+for i in range(100, 1000, 100):
+    averages[i] = 0
+
+for i in range(1000, 10000, 1000):
+    averages[i] = 0
+
+total = 0
+for i in al1:
+    total+=1
+    for j in range(100, 1000, 100):
+        averages[j] += i[j]
+
+    for j in range(1000, 10000, 1000):
+        averages[j] += i[j]
+
+for i in averages:
+    for j in range(100, 400, 100):
+        i[j] = i[j]/total
+
+    for j in range(1000, 10000, 1000):
+        i[j] = i[j]/total
+
+print "Algorithm 1 Timings : ", al1
+print "Algorithm 1 Averages: ", averages
+
+#get averages for alg2
+averages = {}
+for i in range(100, 1000, 100):
+    averages[i] = 0
+
+for i in range(1000, 10000, 1000):
+    averages[i] = 0
+
+total = 0
+for i in al2:
+    total+=1
+    for j in range(100, 1000, 100):
+        averages[j] += i[j]
+
+    for j in range(1000, 10000, 1000):
+        averages[j] += i[j]
+
+for i in averages:
+    for j in range(100, 400, 100):
+        i[j] = i[j]/total
+
+    for j in range(1000, 10000, 1000):
+        i[j] = i[j]/total
+
+print "Algorithm 2 Timings : ", al2
+print "Algorithm 2 Averages: ", averages
+
+#get averages for alg3
+averages = {}
+for i in range(100, 1000, 100):
+    averages[i] = 0
+
+for i in range(1000, 10000, 1000):
+    averages[i] = 0
+
+total = 0
+for i in al3:
+    total+=1
+    for j in range(100, 1000, 100):
+        averages[j] += i[j]
+
+    for j in range(1000, 10000, 1000):
+        averages[j] += i[j]
+
+for i in averages:
+    for j in range(100, 400, 100):
+        i[j] = i[j]/total
+
+    for j in range(1000, 10000, 1000):
+        i[j] = i[j]/total
+
+print "Algorithm 3 Timings : ", al3
+print "Algorithm 3 Averages: ", averages
+
